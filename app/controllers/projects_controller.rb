5ae7c52e22f3479899e4e5cc95d4dcde840e668a
Jefferson Denilson <jefersond121212@gmail.com>
2021-10-10 09:48:11 -0300

    evitando que o usuario acesse projetos que não criou

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,13 @@
 class ProjectsController < ApplicationController
-  # TODO: só mostrar e editar projetos criados pelo usuário
   before_action :authenticate_user!
 
   def show
     @project = Project.find_by(id: params[:id], creator: current_user)
-    redirect_to root_path if @project.nil?
+
+    if @project.nil?
+      flash[:alert] = 'O projeto não foi encontrado'
+      redirect_to root_path
+    end
   end
 
   def new
@@ -24,11 +27,21 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Project.find(params[:id])
+    @project = Project.find_by(id: params[:id], creator: current_user)
+
+    if @project.nil?
+      flash[:alert] = 'O projeto não foi encontrado'
+      redirect_to root_path
+    end
   end
 
   def update
-    @project = Project.find(params[:id])
+    @project = Project.find_by(id: params[:id], creator: current_user)
+
+    if @project.nil?
+      flash[:alert] = 'O projeto não foi encontrado'
+      redirect_to root_path and return
+    end
 
     if @project.update(project_params)
       flash[:notice] = 'Projeto atualizado com sucesso'
