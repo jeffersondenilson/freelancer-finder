e370dec9cac4c4f634bec12976ac6b17fc4f9617
Jefferson Denilson <jefersond121212@gmail.com>
2021-10-21 16:34:54 -0300

    estilos nas telas de sign up e sign in

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap/dist/js/bootstrap.js"
+import "bootstrap-icons/font/fonts/bootstrap-icons.woff"
+import "bootstrap-icons/font/fonts/bootstrap-icons.woff2"
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 Rails.start()
 Turbolinks.start()
