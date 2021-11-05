5626f92dbbfecf52e015a203d859604f38440516
Jefferson Denilson <jefersond121212@gmail.com>
2021-11-05 11:04:20 -0300

    filtros adicionados ao simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  # add_filter '/helpers/'
+  add_filter '/channels/'
+  add_filter '/jobs/'
+  add_filter '/controllers/professionals/'
+  add_filter '/controllers/users/'
+end
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
