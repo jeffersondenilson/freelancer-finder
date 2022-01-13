737391448e7bebdf252ba2a00c51b4a694b4bec1
Jefferson Denilson <jefersond121212@gmail.com>
2022-01-12 21:00:27 -0300

    mais testes de recusa de proposta - recusar proposta no perfil de profissional - usuário não vê propostas recusadas - profissional vê propostas recusadas - recusar proposta aprovada - não recusar proposta cancelada após ser aprovada

--- spec/system/user/user_refuses_proposal_spec.rb
+++ spec/system/user/user_refuses_proposal_spec.rb
@@ -32,4 +32,97 @@ describe 'User refuses proposal' do
     expect(current_path).to eq('/projects/1')
     expect(page).to have_content('Esse projeto ainda não recebeu propostas')
   end
+
+  it 'and refuses proposal at professional profile' do
+    project = create(:project)
+    proposal = create(:proposal, project: project)
+    login_as project.creator, scope: :user
+
+    visit professional_path(proposal.professional)
+    within '#proposal-1' do
+      click_on 'Recusar'
+    end
+    fill_in 'Informe por que quer recusar a proposta:', with: 'Recusando'
+    click_on 'Recusar proposta'
+
+    expect(Proposal.first.status).to eq('refused')
+    expect(ProposalRefusal.first.refuse_reason).to eq('Recusando')
+    expect(current_path).to eq('/projects/1')
+    expect(page).to have_content('Esse projeto ainda não recebeu propostas')
+  end
+
+  it 'and should not see refused proposal' do
+    project = create(:project)
+    refused_proposal = create(
+      :proposal, project: project, message: 'This is refused'
+    )
+    refused_proposal.refused!
+    ProposalRefusal.create!(
+      proposal: refused_proposal, refuse_reason: 'Refusado!'
+    )
+    
+    another_proposal = create(:proposal, project: project, message: 'Pending')
+    
+    login_as project.creator, scope: :user
+
+    visit project_path(project)
+    within '#proposal-2' do
+      expect(page).to have_content("Mensagem: #{another_proposal.message}")
+    end
+    expect(page).not_to have_content("Mensagem: #{refused_proposal.message}")
+  end
+
+  it 'and professional see refused proposal' do
+    project = create(:project)
+    proposal = create(:proposal, project: project)
+    proposal.refused!
+    ProposalRefusal.create!(proposal: proposal, refuse_reason: 'Refusado!')
+
+    login_as proposal.professional, scope: :professional
+
+    visit project_path(project)
+    expect(page).to have_content('Sua proposta')
+    within '#proposal-1' do
+      expect(page).to have_content("Mensagem: #{proposal.message}")
+      expect(page).to have_content("Status: Recusada")
+      expect(page).to have_content(
+        "Motivo de recusa: #{proposal.proposal_refusal.refuse_reason}"
+      )
+    end
+    expect(page).not_to have_link('Editar')
+    expect(page).not_to have_link('Fazer proposta')
+  end
+
+  it 'and should refuse approved proposal' do
+    project = create(:project)
+    proposal = create(:proposal, project: project)
+    proposal.approved!
+    login_as project.creator, scope: :user
+
+    visit project_path(project)
+    within '#proposal-1' do
+      click_on 'Recusar'
+    end
+    fill_in 'Informe por que quer recusar a proposta:', with: 'Recusando'
+    click_on 'Recusar proposta'
+
+    expect(Proposal.first.status).to eq('refused')
+    expect(ProposalRefusal.first.refuse_reason).to eq('Recusando')
+    expect(current_path).to eq('/projects/1')
+    expect(page).to have_content('Esse projeto ainda não recebeu propostas')
+  end
+
+  it 'and should not refuse canceled_approved proposal' do
+    project = create(:project)
+    proposal = create(:proposal, project: project)
+    proposal.canceled_approved!
+    ProposalCancelation.create!(proposal: proposal, cancel_reason: 'Canceled')
+    login_as project.creator, scope: :user
+
+    visit project_path(project)
+    within '#proposal-1' do
+      expect(page).not_to have_link('Recusar')
+      expect(page).to have_content('Motivo de cancelamento: Canceled')
+    end
+  end
 end
